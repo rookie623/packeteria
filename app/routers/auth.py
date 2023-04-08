5e1485b9cf9e979b1542a61e55fe8e003ed7feee
Matias Menoni <rookiemat@gmail.com>
2023-04-07 22:12:35 -0300

    JWT authentication method

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authenticaction"])
 
@@ -8,11 +8,12 @@ router = APIRouter(tags=["Authenticaction"])
 @router.post("/login")
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.username == user_credentials.username).first()
-    print(user)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
 
     if not utils.verifyer(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
 
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
