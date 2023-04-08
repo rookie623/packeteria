d6f315e9cbeafaf2dab4280f95086b1486c71eac
Matias Menoni <rookiemat@gmail.com>
2023-04-07 22:22:13 -0300

    adding OAuth2PassordRequestForm implementation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authenticaction"])
 
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+
     user = db.query(models.User).filter(models.User.username == user_credentials.username).first()
+
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
 
